936d38fbff52439eef491751da82d83cb93a6b39
Ryan Martin <rdm1@ualberta.ca>
2019-01-31 22:03:35 -0700

    change data around

--- convert_toggl/convert_toggl.py
+++ convert_toggl/convert_toggl.py
@@ -12,7 +12,7 @@ def convert_toggl(toggl_export, csvoutfile):
         dfslice = df.loc[df['Start date'] == date]
         hours_worked = dfslice['Duration'].sum()
         tasks = ', '.join([task for task in set(dfslice['Description'])]).capitalize()
-        data.append([date, myname, tasks, hours_worked.total_seconds() / 3600])
+        data.append([date, myname, hours_worked.total_seconds() / 3600, tasks])
     hourtable = pd.DataFrame(data, columns=['Date', 'Employee', 'Hours', 'Description'])
     hourtable.to_csv(csvoutfile, index=False)
 
