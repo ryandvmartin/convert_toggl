518c51c85c1a619b82492290c3600713b8667910
Ryan Martin <rdm1@ualberta.ca>
2019-07-01 13:55:02 -0600

    updated to split time entries by project and export separate tables

--- convert_toggl/convert_toggl.py
+++ convert_toggl/convert_toggl.py
@@ -4,8 +4,7 @@ import argparse
 myname = 'Ryan Martin'
 
 
-def convert_toggl(toggl_export, csvoutfile):
-    df = pd.read_csv(toggl_export)
+def convert_merge_time_entries(df):
     df['Duration'] = df['Duration'].apply(pd.Timedelta).dt.round('15min')
     data = []
     for date in sorted(set(df['Start date'])):
@@ -13,8 +12,18 @@ def convert_toggl(toggl_export, csvoutfile):
         hours_worked = dfslice['Duration'].sum()
         tasks = ', '.join([task for task in set(dfslice['Description'])]).capitalize()
         data.append([date, myname, hours_worked.total_seconds() / 3600, tasks])
-    hourtable = pd.DataFrame(data, columns=['Date', 'Employee', 'Hours', 'Description'])
-    hourtable.to_csv(csvoutfile, index=False)
+    return pd.DataFrame(data, columns=['Date', 'Employee', 'Hours', 'Description'])
+
+
+def convert_toggl_export(toggl_export, outfile):
+    df = pd.read_csv(toggl_export)
+    if 'project' in [c.lower() for c in df.columns]:
+        for projectid, subdf in list(df.groupby('Project')):
+            out_table = convert_merge_time_entries(subdf)
+            out_table.to_csv(f'{projectid.lower()}-{outfile}', index=False)
+    else:
+        out_table = convert_merge_time_entries(df)
+        out_table.to_csv(outfile, index=False)
 
 
 def main():
@@ -23,7 +32,7 @@ def main():
     parser.add_argument('processed_csv', nargs='?', type=str, default='out.csv',
                         help='The output csv file')
     args = parser.parse_args()
-    convert_toggl(args.toggle_export_csv, args.processed_csv)
+    convert_toggl_export(args.toggle_export_csv, args.processed_csv)
 
 
 if __name__ == '__main__':
