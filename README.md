f6c2dbe14613b243658e3c3c97bbce69bb34e0d5
rdmar <rdm1@ualberta.ca>
2019-09-01 19:28:54 -0600

    udpates for automtically exporting pdf reports with basic formatting

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# convert_toggl
+# ``convert_toggl``
 
-Utility to process toggl csv output into billable timesheets. Collects multiple entries by day into a single entry, rounded to 15 minute intervals.
+Utility to process ``toggl`` csv output into timesheets. Collects multiple entries by day into a single entry, rounded to 15 minute intervals.
 
 Installation:
 
 1. Clone Repo
 
-2. Modify `myname` in `convert_toggl/convert_toggl.py`
+2. If exporting pdf's, follow instructions for [WeazyPrint](https://weasyprint.readthedocs.io/en/latest/install.html#windows).
 
 3. Install to local python distribution:
 
@@ -16,8 +16,16 @@ Installation:
 
         > pip install -e .
 
+4. Configure your name for the exported reports:
+
+    > convert_toggl -name "Ryan Martin"
+
+5. Export a ``.csv`` from the detailed summary, e.g., for last month
+
     Usage:
 
         > convert_toggl detailed_toggl_export.csv
 
-Currently separate tables are export by `Project` field.
+    Optionally, with a set of pdf's:
+
+        > convert_toggl -topdf detailed_toggl_export.csv
--- convert_toggl/convert_toggl.py
+++ convert_toggl/convert_toggl.py
@@ -3,18 +3,44 @@ import argparse
 import os
 
 import pandas as pd
+pd.set_option('display.max_colwidth', -1)
+
+THISDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def save_table(data: pd.DataFrame, outfile, name=None, project=None):
+    """Render a table to pdf using jinja2 templates"""
+    from jinja2 import Environment, FileSystemLoader
+    from weasyprint import HTML
+    env = Environment(loader=FileSystemLoader(THISDIR))
+    template = env.get_template('table_template.html')
+    if name is None:
+        name = get_name()
+    if project:
+        table_title = f'{name} - Hours - {project}'
+    else:
+        table_title = f'{name} - Hours'
+    data = data[['Date', 'Employee', 'Hours', 'Description']]
+    html_table = data.to_html(index=False, float_format='%.2f',
+                              justify='left')
+    template_vars = {'title': 'Time Sheet',
+                     'table_title': table_title,
+                     'hours_table': html_table}
+    html_out = template.render(template_vars)
+    HTML(string=html_out).write_pdf(outfile, stylesheets=[
+        os.path.join(THISDIR, 'style.css')])
 
 
 def set_name(name):
     """Set ``name`` to disk"""
-    with open(os.path.join(os.path.dirname(__file__), 'name.txt'), 'w') as fh:
+    with open(os.path.join(THISDIR, 'name.txt'), 'w') as fh:
         fh.write(name.strip())
 
 
 def get_name():
     """Get a name from the stored file"""
     try:
-        with open(os.path.join(os.path.dirname(__file__), 'name.txt'), 'r') as fh:
+        with open(os.path.join(THISDIR, 'name.txt'), 'r') as fh:
             return fh.read().strip()
     except Exception:
         set_name("MY NAME")
@@ -43,16 +69,21 @@ def convert_merge_time_entries(df: pd.DataFrame):
                                        'Description'])
 
 
-def convert_toggl_export(toggl_export, outfile):
+def convert_toggl_export(toggl_export, outfile, topdf=False):
     """Read through an exported toggle file and save by project"""
     df = pd.read_csv(toggl_export)
     if 'project' in [c.lower() for c in df.columns]:
         for projectid, subdf in list(df.groupby('Project')):
             out_table = convert_merge_time_entries(subdf)
             out_table.to_csv(f'{projectid.lower()}-{outfile}', index=False)
+            if topdf:
+                save_table(out_table, f'{projectid.lower()}-{outfile}.pdf',
+                           project=projectid)
     else:
         out_table = convert_merge_time_entries(df)
         out_table.to_csv(outfile, index=False)
+        if topdf:
+            save_table(out_table, f'{projectid.lower()}-{outfile}.pdf')
 
 
 def main():
@@ -64,13 +95,13 @@ def main():
                         help='The output csv file')
     parser.add_argument('-name', type=str, nargs='?', default=None,
                         help='Set a default name with "MY NAME"')
-    parser.add_argument('-to-pdf', action='store_true', help='Export a formatted pdf')
+    parser.add_argument('-topdf', action='store_true', help='Export a formatted pdf')
     args = parser.parse_args()
     if args.name:
         print(f"Setting ``NAME`` == '{args.name}'")
         set_name(args.name)
     else:
-        convert_toggl_export(args.toggle_export_csv, args.processed_csv)
+        convert_toggl_export(args.toggle_export_csv, args.processed_csv, args.topdf)
 
 
 if __name__ == '__main__':
