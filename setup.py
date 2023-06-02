b738c062d3c02d0fcd4ac66f647f1f1ad66cffdf
Ryan Martin <ryan.martin@resmodsol.com>
2023-06-01 21:52:16 -0400

    bump version and add xlsxwriter to pip requirements

--- setup.py
+++ setup.py
@@ -6,20 +6,22 @@ Convert Toggl (c) Ryan Martin 2019
 from setuptools import setup
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    setup(name='convert_toggl',
-          version='0.0.2',
-          description='Convert toggl csv-dumps to nice formatted spreadsheets',
-          maintainer='Ryan Martin',
-          maintainer_email='rdm1@ualberta.ca',
-          author=['Ryan Martin'],
-          license='MIT / CCG',
-          packages=['convert_toggl'],
-          package_requires=["python=3", "pandas"],
-          entry_points={
-              'console_scripts': [
-                  'convert_toggl=convert_toggl:main',
-              ]
-          },
-          zip_safe=False)
+    setup(
+        name="convert_toggl",
+        version="0.0.3",
+        description="Convert toggl csv-dumps to nice formatted spreadsheets",
+        maintainer="Ryan Martin",
+        maintainer_email="rdm1@ualberta.ca",
+        author=["Ryan Martin"],
+        license="MIT / CCG",
+        packages=["convert_toggl"],
+        package_requires=["python=3", "pandas", "xlsxwriter"],
+        entry_points={
+            "console_scripts": [
+                "convert_toggl=convert_toggl:main",
+            ]
+        },
+        zip_safe=False,
+    )
